26e419d8150a4e3b4418c6687b0ab48e0c736b20
QuangBaBop <phanduccongquang6501@gmail.com>
2024-08-05 12:21:16 +0700

    update

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000/v1",
+                url: "https://localhost:3000/v1",
             },
         ],
         components: {
